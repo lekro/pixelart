9d7a93f79258be6ea8253195d1ddba6f85863335
lekro <kapuraya@gmail.com>
2017-08-27 00:55:33 -0500

    Fix dependencies and increment version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
         name='pixelart-helper',
-        version='0.0.2',
+        version='0.0.3',
         description='Match images to minecraft textures',
         long_description=long_description,
         url='https://github.com/lekro/pixelart-helper',
@@ -29,7 +29,7 @@ setup(
         ],
         keywords=['pixel', 'art', 'pixelart', 'minecraft'],
         packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-        install_requires=['numpy', 'scipy', 'pillow'],
+        install_requires=['numpy', 'scipy', 'Pillow'],
         python_requires='>=3',
         entry_points={
             'console_scripts': [
